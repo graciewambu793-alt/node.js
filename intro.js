0cfd7035b7b4a5ec1bf1e900c8c7cf7a0f4dd4a0
graciewambu793-alt <graciewambu793@gmail.com>
2026-03-14 21:50:14 +0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Grace Wambui";
+const age = 23;
+const favoriteLanguage = "Python";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorite Programming Language: " + favoriteLanguage);
